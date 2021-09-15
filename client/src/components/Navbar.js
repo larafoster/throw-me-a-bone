5f7f892fccb1cca356cba4ff387c523e905159be
lara <lara@vehicle4change.org>
2021-09-15 13:24:56 -0400

    added title to Navbar view

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -21,7 +21,7 @@ const AppNavbar = () => {
           <Navbar.Collapse id='navbar'>
             <Nav className='ml-auto'>
               <Nav.Link as={Link} to='/'>
-                Search For Books
+                Throw Me A Bone!
               </Nav.Link>
               {/* if user is logged in show saved books and logout */}
               {Auth.loggedIn() ? (
