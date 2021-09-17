2bb4bf0e293f09e3c65763ceb864921fdd2970ef
Carsons Mac <carsoncoyle2015@gmail.com>
2021-09-17 14:15:42 -0400

    added state to adoption form

--- client/src/components/adoptionForm.js
+++ client/src/components/adoptionForm.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Container, Card, Col, Button, Form, FormGroup, Label, Input } from 'reactstrap';
 import { useMutation } from '@apollo/client';
+import { ADD_FORM } from '../utils/mutations'
 
 
 
@@ -16,7 +17,7 @@ const AdoptForm = () => {
     dogName: '',
     message: ''
   })
-  const [addData, { error }] = useMutation()
+  const [addData, { error }] = useMutation(ADD_FORM)
   const handleInput = (e) => {
     const { name, value } = e.target
     setUserFormData({...userFormData, [name]: value})
@@ -34,61 +35,61 @@ const AdoptForm = () => {
       console.log(err)
   }}
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Container>
         <Card>
           <FormGroup row>
             <Label for="exampleEmail">First Name</Label>
             <Col sm={10}>
-            <Input type="fname" name="firstName" id="First" placeholder="First name" />
+            <Input type="fname" name="firstName" id="First" value={userFormData.firstName} placeholder="First name" />
             </Col>
           </FormGroup>
           <FormGroup row>
             <Label for="exampleEmail">Last Name</Label>
             <Col sm={10}>
-            <Input type="lname" name="lastName" id="Last" placeholder="Last name" />
+            <Input type="lname" name="lastName" id="Last" value={userFormData.lastName} placeholder="Last name" />
             </Col>
             </FormGroup>
           <FormGroup row>
             <Label for="exampleEmail">Street Address</Label>
             <Col sm={10}>
-            <Input type="address" name="street" id="Address" placeholder="Street address" />
+            <Input type="address" name="street" id="Address" value={userFormData.street} placeholder="Street address" />
             </Col>
             </FormGroup>
           <FormGroup row>
             <Label for="exampleEmail">City</Label>
             <Col sm={10}>
-            <Input type="city" name="city" id="City" placeholder="City" />
+            <Input type="city" name="city" id="City" value={userFormData.city} placeholder="City" />
             </Col>
             </FormGroup>
           <FormGroup row>
             <Label for="exampleEmail">State</Label>
             <Col sm={10}>
-            <Input type="stateL" name="userState" id="State" placeholder="State" />
+            <Input type="stateL" name="userState" id="State" value={userFormData.userState} placeholder="State" />
             </Col>
             </FormGroup>
           <FormGroup row>
             <Label for="exampleEmail">Zip Code</Label>
             <Col sm={10}>
-            <Input type="zip" name="zip" id="Zip" placeholder="Zip code" />
+            <Input type="zip" name="zip" id="Zip" value={userFormData.zip} placeholder="Zip code" />
             </Col>
             </FormGroup>
           <FormGroup row>
             <Label for="exampleEmail">Email</Label>
             <Col sm={10}>
-            <Input type="nemail" name="email" id="Email" placeholder="Email" />
+            <Input type="nemail" name="email" id="Email" value={userFormData.email} placeholder="Email" />
             </Col>
             </FormGroup>
           <FormGroup row>
             <Label for="exampleEmail">Name of dog that you're interested in</Label>
             <Col sm={10}>
-            <Input type="dogname" name="dogName" id="Dog" placeholder="Name of dog" />
+            <Input type="dogname" name="dogName" id="Dog" value={userFormData.dogName} placeholder="Name of dog" />
             </Col>
             </FormGroup>
           <FormGroup row>
           <Label for="exampleText">Is there anything we need to know about your home to ensure this adoption would be a good fit?</Label>
           <Col sm={10}>
-          <Input type="textarea" name="message" id="Info" />
+          <Input type="textarea" name="message" value={userFormData.message} id="Info" />
           </Col>
           </FormGroup>
           <Button row center>Proceed to checkout!</Button>
